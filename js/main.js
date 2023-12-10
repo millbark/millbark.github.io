7a0f41a6f42faec7aa3e113e4c15b9f6961614de
millbark <149570355+millbark@users.noreply.github.com>
2023-12-11 03:31:10 +0900

    모멘텀 완성본

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 //랜덤 배경화면
-const images=["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg"];
+const images=["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg"];
 const randomImage=images[Math.floor(Math.random()*images.length)]
 const imageBg =document.createElement('img');
 imageBg.src=`images/${randomImage}`;
